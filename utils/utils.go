99b1b872b90dd9421c2839ae0ad209a1c70aff75
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-02-23 14:17:56 +0800

    添加x509测试用例

--- utils/utils.go
+++ utils/utils.go
@@ -2,6 +2,8 @@ package utils
 
 import (
 	"bytes"
+	"crypto/ecdsa"
+	"crypto/elliptic"
 	"crypto/rand"
 	"encoding/asn1"
 	"encoding/pem"
@@ -132,4 +134,66 @@ func UnmarshalECSignature(raw []byte) (*big.Int, *big.Int, error) {
 	}
 
 	return sig.R, sig.S, nil
+}
+
+var (
+	// curveHalfOrders contains the precomputed curve group orders halved.
+	// It is used to ensure that signature' S value is lower or equal to the
+	// curve group order halved. We accept only low-S signatures.
+	// They are precomputed for efficiency reasons.
+	curveHalfOrders = map[elliptic.Curve]*big.Int{
+		elliptic.P224(): new(big.Int).Rsh(elliptic.P224().Params().N, 1),
+		elliptic.P256(): new(big.Int).Rsh(elliptic.P256().Params().N, 1),
+		elliptic.P384(): new(big.Int).Rsh(elliptic.P384().Params().N, 1),
+		elliptic.P521(): new(big.Int).Rsh(elliptic.P521().Params().N, 1),
+	}
+)
+
+//goland:noinspection GoUnusedExportedFunction
+func GetCurveHalfOrdersAt(c elliptic.Curve) *big.Int {
+	return big.NewInt(0).Set(curveHalfOrders[c])
+}
+
+// SignatureToLowS 检查ecdsa签名的s值是否是lower-s值，如果不是，则将s转为对应的lower-s值并重新序列化为ecdsa签名
+//goland:noinspection GoUnusedExportedFunction
+func SignatureToLowS(k *ecdsa.PublicKey, signature []byte) ([]byte, error) {
+	r, s, err := UnmarshalECSignature(signature)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err = ToLowS(k, s)
+	if err != nil {
+		return nil, err
+	}
+
+	return MarshalECSignature(r, s)
+}
+
+// IsLowS checks that s is a low-S
+func IsLowS(k *ecdsa.PublicKey, s *big.Int) (bool, error) {
+	halfOrder, ok := curveHalfOrders[k.Curve]
+	if !ok {
+		return false, fmt.Errorf("curve not recognized [%s]", k.Curve)
+	}
+
+	return s.Cmp(halfOrder) != 1, nil
+
+}
+
+func ToLowS(k *ecdsa.PublicKey, s *big.Int) (*big.Int, error) {
+	lowS, err := IsLowS(k, s)
+	if err != nil {
+		return nil, err
+	}
+
+	if !lowS {
+		// Set s to N - s that will be then in the lower part of signature space
+		// less or equal to half order
+		s.Sub(k.Params().N, s)
+
+		return s, nil
+	}
+
+	return s, nil
 }
