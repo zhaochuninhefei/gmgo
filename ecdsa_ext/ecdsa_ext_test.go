d6e005af8771e0e2caf830d3f7a8ef10b28e5462
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-03-02 10:30:38 +0800

    扩展ecdsa

--- ecdsa_ext/ecdsa_ext_test.go
+++ ecdsa_ext/ecdsa_ext_test.go
@@ -1,3 +1,11 @@
+// Copyright (c) 2023 zhaochun
+// gmgo is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//          http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
 package ecdsa_ext
 
 import (
@@ -6,11 +14,13 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
-	"gitee.com/zhaochuninhefei/gmgo/ecbase"
+	"gitee.com/zhaochuninhefei/zcgolog/zclog"
 	"testing"
 )
 
 func TestPrivateKey_Sign(t *testing.T) {
+	zclog.Level = zclog.LOG_LEVEL_DEBUG
+
 	privateKey, err := GenerateKey(elliptic.P256(), rand.Reader)
 	if err != nil {
 		t.Fatal(err)
@@ -21,7 +31,7 @@ func TestPrivateKey_Sign(t *testing.T) {
 	fmt.Printf("msg: %s\n", msg)
 	fmt.Printf("digest hex: %s\n", hex.EncodeToString(digest[:]))
 
-	sign, err := privateKey.Sign(rand.Reader, digest[:], ecbase.CreateDefaultEcSignerOpts())
+	sign, err := privateKey.Sign(rand.Reader, digest[:], nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -31,7 +41,7 @@ func TestPrivateKey_Sign(t *testing.T) {
 	if !ok {
 		t.Fatal("PublicKey类型强转失败")
 	}
-	valied := pubKey.Verify(digest[:], sign)
+	valied, err := pubKey.EcVerify(digest[:], sign, nil)
 	if !valied {
 		t.Fatal("验签失败")
 	}
