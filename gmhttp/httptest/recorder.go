64aca3504eb6aa1938226320b4965f99d2350795
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-04-25 08:34:55 +0800

    优化代码

--- gmhttp/httptest/recorder.go
+++ gmhttp/httptest/recorder.go
@@ -31,7 +31,7 @@ type ResponseRecorder struct {
 	// HeaderMap contains the headers explicitly set by the Handler.
 	// It is an internal detail.
 	//
-	// Deprecated: HeaderMap exists for historical compatibility
+	// ToDeprecated: HeaderMap exists for historical compatibility
 	// and should not be used. To access the headers returned by a handler,
 	// use the Response.Header map as returned by the Result method.
 	HeaderMap http.Header
