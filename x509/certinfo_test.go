ab454c6f4bd7c31c74a868acac5a2291b2645707
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-05-06 08:36:44 +0800

    优化代码

--- x509/certinfo_test.go
+++ x509/certinfo_test.go
@@ -2,9 +2,8 @@ package x509_test
 
 import (
 	"fmt"
-	"testing"
-
 	"gitee.com/zhaochuninhefei/gmgo/x509"
+	"testing"
 )
 
 func TestCertinfo(t *testing.T) {
@@ -17,4 +16,43 @@ func TestCertinfo(t *testing.T) {
 		t.Fatal(err)
 	}
 	fmt.Println(certText)
+}
+
+func TestCertificateText(t *testing.T) {
+	type args struct {
+		certPath string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "sm2 sign cert",
+			args: args{
+				certPath: "testdata/sm2_sign_cert.cer",
+			},
+		},
+		{
+			name: "ecdsa sign cert",
+			args: args{
+				certPath: "testdata/ecdsa_sign_cert.cer",
+			},
+		},
+		{
+			name: "ecdsaext sign cert",
+			args: args{
+				certPath: "testdata/ecdsaext_sign_cert.cer",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cert, _ := x509.ReadCertificateFromPemFile(tt.args.certPath)
+			certText, err := x509.CertificateText(cert)
+			if err != nil {
+				t.Fatal(err)
+			}
+			fmt.Println(certText)
+		})
+	}
 }
