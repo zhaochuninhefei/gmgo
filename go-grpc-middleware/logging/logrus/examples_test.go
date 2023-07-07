b13c143de712cfe4a746901a9cf205a1dc64d161
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-07-07 10:08:54 +0800

    优化代码

--- go-grpc-middleware/logging/logrus/examples_test.go
+++ go-grpc-middleware/logging/logrus/examples_test.go
@@ -4,10 +4,10 @@ import (
 	"gitee.com/zhaochuninhefei/gmgo/go-grpc-middleware/logging/logrus/ctxlogrus"
 	"time"
 
-	grpc_middleware "gitee.com/zhaochuninhefei/gmgo/go-grpc-middleware"
-	grpc_logrus "gitee.com/zhaochuninhefei/gmgo/go-grpc-middleware/logging/logrus"
-	grpc_ctxtags "gitee.com/zhaochuninhefei/gmgo/go-grpc-middleware/tags"
-	pb_testproto "gitee.com/zhaochuninhefei/gmgo/go-grpc-middleware/testing/testproto"
+	grpcmiddleware "gitee.com/zhaochuninhefei/gmgo/go-grpc-middleware"
+	grpclogrus "gitee.com/zhaochuninhefei/gmgo/go-grpc-middleware/logging/logrus"
+	grpcctxtags "gitee.com/zhaochuninhefei/gmgo/go-grpc-middleware/tags"
+	pbtestproto "gitee.com/zhaochuninhefei/gmgo/go-grpc-middleware/testing/testproto"
 	"gitee.com/zhaochuninhefei/gmgo/grpc"
 	"gitee.com/zhaochuninhefei/gmgo/net/context"
 	"github.com/sirupsen/logrus"
@@ -15,7 +15,7 @@ import (
 
 var (
 	logrusLogger *logrus.Logger
-	customFunc   grpc_logrus.CodeToLevel
+	customFunc   grpclogrus.CodeToLevel
 )
 
 // Initialization shows a relatively complex initialization sequence.
@@ -23,20 +23,20 @@ func Example_initialization() {
 	// Logrus entry is used, allowing pre-definition of certain fields by the user.
 	logrusEntry := logrus.NewEntry(logrusLogger)
 	// Shared options for the logger, with a custom gRPC code to log level function.
-	opts := []grpc_logrus.Option{
-		grpc_logrus.WithLevels(customFunc),
+	opts := []grpclogrus.Option{
+		grpclogrus.WithLevels(customFunc),
 	}
 	// Make sure that log statements internal to gRPC library are logged using the logrus Logger as well.
-	grpc_logrus.ReplaceGrpcLogger(logrusEntry)
+	grpclogrus.ReplaceGrpcLogger(logrusEntry)
 	// Create a server, make sure we put the grpc_ctxtags context before everything else.
 	_ = grpc.NewServer(
-		grpc_middleware.WithUnaryServerChain(
-			grpc_ctxtags.UnaryServerInterceptor(grpc_ctxtags.WithFieldExtractor(grpc_ctxtags.CodeGenRequestFieldExtractor)),
-			grpc_logrus.UnaryServerInterceptor(logrusEntry, opts...),
+		grpcmiddleware.WithUnaryServerChain(
+			grpcctxtags.UnaryServerInterceptor(grpcctxtags.WithFieldExtractor(grpcctxtags.CodeGenRequestFieldExtractor)),
+			grpclogrus.UnaryServerInterceptor(logrusEntry, opts...),
 		),
-		grpc_middleware.WithStreamServerChain(
-			grpc_ctxtags.StreamServerInterceptor(grpc_ctxtags.WithFieldExtractor(grpc_ctxtags.CodeGenRequestFieldExtractor)),
-			grpc_logrus.StreamServerInterceptor(logrusEntry, opts...),
+		grpcmiddleware.WithStreamServerChain(
+			grpcctxtags.StreamServerInterceptor(grpcctxtags.WithFieldExtractor(grpcctxtags.CodeGenRequestFieldExtractor)),
+			grpclogrus.StreamServerInterceptor(logrusEntry, opts...),
 		),
 	)
 }
@@ -45,41 +45,41 @@ func Example_initializationWithDurationFieldOverride() {
 	// Logrus entry is used, allowing pre-definition of certain fields by the user.
 	logrusEntry := logrus.NewEntry(logrusLogger)
 	// Shared options for the logger, with a custom duration to log field function.
-	opts := []grpc_logrus.Option{
-		grpc_logrus.WithDurationField(func(duration time.Duration) (key string, value interface{}) {
+	opts := []grpclogrus.Option{
+		grpclogrus.WithDurationField(func(duration time.Duration) (key string, value interface{}) {
 			return "grpc.time_ns", duration.Nanoseconds()
 		}),
 	}
 	_ = grpc.NewServer(
-		grpc_middleware.WithUnaryServerChain(
-			grpc_ctxtags.UnaryServerInterceptor(),
-			grpc_logrus.UnaryServerInterceptor(logrusEntry, opts...),
+		grpcmiddleware.WithUnaryServerChain(
+			grpcctxtags.UnaryServerInterceptor(),
+			grpclogrus.UnaryServerInterceptor(logrusEntry, opts...),
 		),
-		grpc_middleware.WithStreamServerChain(
-			grpc_ctxtags.StreamServerInterceptor(),
-			grpc_logrus.StreamServerInterceptor(logrusEntry, opts...),
+		grpcmiddleware.WithStreamServerChain(
+			grpcctxtags.StreamServerInterceptor(),
+			grpclogrus.StreamServerInterceptor(logrusEntry, opts...),
 		),
 	)
 }
 
 // Simple unary handler that adds custom fields to the requests's context. These will be used for all log statements.
 func ExampleExtract_unary() {
-	_ = func(ctx context.Context, ping *pb_testproto.PingRequest) (*pb_testproto.PingResponse, error) {
+	_ = func(ctx context.Context, ping *pbtestproto.PingRequest) (*pbtestproto.PingResponse, error) {
 		// Add fields the ctxtags of the request which will be added to all extracted loggers.
-		grpc_ctxtags.Extract(ctx).Set("custom_tags.string", "something").Set("custom_tags.int", 1337)
+		grpcctxtags.Extract(ctx).Set("custom_tags.string", "something").Set("custom_tags.int", 1337)
 		// Extract a single request-scoped logrus.Logger and log messages.
 		// ctx_logrus.Extract is deprecated, use the ctxlogrus.Extract instead.
 		//l := ctx_logrus.Extract(ctx)
 		l := ctxlogrus.Extract(ctx)
 		l.Info("some ping")
 		l.Info("another ping")
-		return &pb_testproto.PingResponse{Value: ping.Value}, nil
+		return &pbtestproto.PingResponse{Value: ping.Value}, nil
 	}
 }
 
 func ExampleWithDecider() {
-	opts := []grpc_logrus.Option{
-		grpc_logrus.WithDecider(func(methodFullName string, err error) bool {
+	opts := []grpclogrus.Option{
+		grpclogrus.WithDecider(func(methodFullName string, err error) bool {
 			// will not log gRPC calls if it was a call to healthcheck and no error was raised
 			if err == nil && methodFullName == "blah.foo.healthcheck" {
 				return false
@@ -91,11 +91,11 @@ func ExampleWithDecider() {
 	}
 
 	_ = []grpc.ServerOption{
-		grpc_middleware.WithStreamServerChain(
-			grpc_ctxtags.StreamServerInterceptor(),
-			grpc_logrus.StreamServerInterceptor(logrus.NewEntry(logrus.New()), opts...)),
-		grpc_middleware.WithUnaryServerChain(
-			grpc_ctxtags.UnaryServerInterceptor(),
-			grpc_logrus.UnaryServerInterceptor(logrus.NewEntry(logrus.New()), opts...)),
+		grpcmiddleware.WithStreamServerChain(
+			grpcctxtags.StreamServerInterceptor(),
+			grpclogrus.StreamServerInterceptor(logrus.NewEntry(logrus.New()), opts...)),
+		grpcmiddleware.WithUnaryServerChain(
+			grpcctxtags.UnaryServerInterceptor(),
+			grpclogrus.UnaryServerInterceptor(logrus.NewEntry(logrus.New()), opts...)),
 	}
 }
