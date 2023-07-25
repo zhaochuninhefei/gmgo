e2d4e05a198099bdb29a913d1cac8f4265b7d156
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-07-25 09:49:38 +0800

    优化代码

--- go-grpc-middleware/logging/zap/grpclogger.go
+++ go-grpc-middleware/logging/zap/grpclogger.go
@@ -5,7 +5,6 @@ package grpc_zap
 
 import (
 	"fmt"
-
 	"gitee.com/zhaochuninhefei/gmgo/grpc/grpclog"
 	"go.uber.org/zap"
 )
@@ -14,7 +13,9 @@ import (
 // This should be called *before* any other initialization, preferably from init() functions.
 func ReplaceGrpcLogger(logger *zap.Logger) {
 	zgl := &zapGrpcLogger{logger.With(SystemField, zap.Bool("grpc_log", true))}
-	grpclog.SetLogger(zgl)
+	// grpclog.SetLogger is deprecated, use grpclog.SetLoggerV2 instead.
+	//grpclog.SetLogger(zgl)
+	grpclog.SetLoggerV2(zgl)
 }
 
 type zapGrpcLogger struct {
@@ -43,4 +44,46 @@ func (l *zapGrpcLogger) Printf(format string, args ...interface{}) {
 
 func (l *zapGrpcLogger) Println(args ...interface{}) {
 	l.logger.Info(fmt.Sprint(args...))
+}
+
+func (l *zapGrpcLogger) V(le int) bool {
+	// V 接口是检查verbosity level 是否大于等于 请求的 verbose level。
+	// 但 *zap.Logger 没有verbosity level，这里直接返回true。
+	return true
+}
+
+func (l *zapGrpcLogger) Info(args ...interface{}) {
+	l.logger.Info(fmt.Sprint(args...))
+}
+
+func (l *zapGrpcLogger) Infoln(args ...interface{}) {
+	l.logger.Info(fmt.Sprint(args...))
+}
+
+func (l *zapGrpcLogger) Infof(format string, args ...interface{}) {
+	l.logger.Info(fmt.Sprintf(format, args...))
+}
+
+func (l *zapGrpcLogger) Warning(args ...interface{}) {
+	l.logger.Warn(fmt.Sprint(args...))
+}
+
+func (l *zapGrpcLogger) Warningln(args ...interface{}) {
+	l.logger.Warn(fmt.Sprint(args...))
+}
+
+func (l *zapGrpcLogger) Warningf(format string, args ...interface{}) {
+	l.logger.Warn(fmt.Sprintf(format, args...))
+}
+
+func (l *zapGrpcLogger) Error(args ...interface{}) {
+	l.logger.Error(fmt.Sprint(args...))
+}
+
+func (l *zapGrpcLogger) Errorln(args ...interface{}) {
+	l.logger.Error(fmt.Sprint(args...))
+}
+
+func (l *zapGrpcLogger) Errorf(format string, args ...interface{}) {
+	l.logger.Error(fmt.Sprintf(format, args...))
 }
