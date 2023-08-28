218528bf1b117ef07dfda3b3eafa2d416a5670e6
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-08-28 08:59:17 +0800

    优化代码

--- run_test.sh
+++ run_test.sh
@@ -45,6 +45,17 @@ echo
 read -p "x509测试用例 结束，按下任意按键继续..." -n 1
 echo
 
+# x509test
+echo "x509test测试用例"
+cd x509test
+go test
+cd ../
+
+# 等待控制台输入任意字符继续
+echo
+read -p "x509test测试用例 结束，按下任意按键继续..." -n 1
+echo
+
 # gmtls
 echo "gmtls测试用例"
 cd gmtls/tls_test
