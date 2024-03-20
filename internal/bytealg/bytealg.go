f5a660b91b541626d7c2dbff12f1195fbba87a05
zhaochuninhefei <zhaochun.hefei@qq.com>
2024-03-20 13:45:55 +0800

    优化代码

--- internal/bytealg/bytealg.go
+++ internal/bytealg/bytealg.go
@@ -25,6 +25,7 @@ const (
 
 // MaxLen is the maximum length of the string to be searched for (argument b) in Index.
 // If MaxLen is not 0, make sure MaxLen >= 4.
+//goland:noinspection GoUnusedGlobalVariable
 var MaxLen int
 
 // FIXME: the logic of HashStrBytes, HashStrRevBytes, IndexRabinKarpBytes and HashStr, HashStrRev,
