f6970f684db8b840a022a3b6a571414c64515af9
zhaochuninhefei <zhaochun.hefei@qq.com>
2024-04-09 11:45:23 +0800

    移除对废弃模块"github.com/golang/protobuf/jsonpb"的使用

--- go-control-plane/pkg/server/v3/gateway.go
+++ go-control-plane/pkg/server/v3/gateway.go
@@ -15,6 +15,7 @@
 package server
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -84,7 +85,8 @@ func (h *HTTPGateway) ServeHTTP(req *http.Request) ([]byte, int, error) {
 	if err != nil {
 		// SkipFetchErrors will return a 304 which will signify to the envoy client that
 		// it is already at the latest version; all other errors will 500 with a message.
-		if _, ok := err.(*types.SkipFetchError); ok {
+		var skipFetchError *types.SkipFetchError
+		if errors.As(err, &skipFetchError) {
 			return nil, http.StatusNotModified, nil
 		}
 		return nil, http.StatusInternalServerError, fmt.Errorf("fetch error: " + err.Error())
