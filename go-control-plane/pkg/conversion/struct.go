6a7c3212c940d79cdd164330f037a1b2abf406cb
zhaochuninhefei <zhaochun.hefei@qq.com>
2024-04-09 10:46:10 +0800

    修改代码:`go-control-plane/pkg/conversion/struct.go`,移除对废弃模块"github.com/golang/protobuf/jsonpb"的使用

--- go-control-plane/pkg/conversion/struct.go
+++ go-control-plane/pkg/conversion/struct.go
@@ -16,29 +16,43 @@
 package conversion
 
 import (
-	"bytes"
 	"errors"
 
-	"github.com/golang/protobuf/jsonpb"
-	"github.com/golang/protobuf/proto"
-	pstruct "github.com/golang/protobuf/ptypes/struct"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/structpb"
 )
 
 // MessageToStruct encodes a protobuf Message into a Struct. Hilariously, it
 // uses JSON as the intermediary
 // author:glen@turbinelabs.io
-func MessageToStruct(msg proto.Message) (*pstruct.Struct, error) {
+// func MessageToStruct(msg proto.Message) (*pstruct.Struct, error) {
+func MessageToStruct(msg proto.Message) (*structpb.Struct, error) {
 	if msg == nil {
 		return nil, errors.New("nil message")
 	}
 
-	buf := &bytes.Buffer{}
-	if err := (&jsonpb.Marshaler{OrigName: true}).Marshal(buf, msg); err != nil {
+	//	buf := &bytes.Buffer{}
+	//	if err := (&jsonpb.Marshaler{OrigName: true}).Marshal(buf, msg); err != nil {
+	//		return nil, err
+	//	}
+	// 使用 protojson.MarshalOptions 来自定义编marshalling行为，例如OrigName
+	opts := protojson.MarshalOptions{
+		UseProtoNames: true, // 相当于 jsonpb.Marshaler 的 OrigName: true
+	}
+	jsonData, err := opts.Marshal(msg)
+	if err != nil {
 		return nil, err
 	}
 
-	pbs := &pstruct.Struct{}
-	if err := jsonpb.Unmarshal(buf, pbs); err != nil {
+	//	pbs := &pstruct.Struct{}
+	//	if err := jsonpb.Unmarshal(buf, pbs); err != nil {
+	//		return nil, err
+	//	}
+	// 使用 "google.golang.org/protobuf/types/known/structpb" 代替 "github.com/golang/protobuf/ptypes/struct"
+	pbs := &structpb.Struct{}
+	// 使用 protojson.Unmarshal 来替代 jsonpb.Unmarshal
+	if err := protojson.Unmarshal(jsonData, pbs); err != nil {
 		return nil, err
 	}
 
@@ -46,15 +60,26 @@ func MessageToStruct(msg proto.Message) (*pstruct.Struct, error) {
 }
 
 // StructToMessage decodes a protobuf Message from a Struct.
-func StructToMessage(pbst *pstruct.Struct, out proto.Message) error {
+// func StructToMessage(pbst *pstruct.Struct, out proto.Message) error {
+func StructToMessage(pbst *structpb.Struct, out proto.Message) error {
 	if pbst == nil {
 		return errors.New("nil struct")
 	}
 
-	buf := &bytes.Buffer{}
-	if err := (&jsonpb.Marshaler{OrigName: true}).Marshal(buf, pbst); err != nil {
+	//	buf := &bytes.Buffer{}
+	//	if err := (&jsonpb.Marshaler{OrigName: true}).Marshal(buf, pbst); err != nil {
+	//		return err
+	//	}
+	// 使用 protojson.MarshalOptions 来自定义编marshalling行为，例如OrigName
+	opts := protojson.MarshalOptions{
+		UseProtoNames: true, // 相当于 jsonpb.Marshaler 的 OrigName: true
+	}
+	jsonData, err := opts.Marshal(pbst)
+	if err != nil {
 		return err
 	}
 
-	return jsonpb.Unmarshal(buf, out)
+	//	return jsonpb.Unmarshal(buf, out)
+	// 使用 protojson.Unmarshal 来替代 jsonpb.Unmarshal
+	return protojson.Unmarshal(jsonData, out)
 }
