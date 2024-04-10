b241634eb2f9f45db6ed58ad964f07ca32a12203
zhaochuninhefei <zhaochun.hefei@qq.com>
2024-04-10 16:25:34 +0800

    合并google.golang.org/grpc v1.63.2版本代码

--- grpc/stress/grpc_testing/metrics.pb.go
+++ grpc/stress/grpc_testing/metrics.pb.go
@@ -28,7 +28,6 @@
 package grpc_testing
 
 import (
-	proto "github.com/golang/protobuf/proto"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
@@ -41,10 +40,6 @@ const (
 	// Verify that runtime/protoimpl is sufficiently up-to-date.
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
-
-// This is a compile-time assertion that a sufficiently up-to-date version
-// of the legacy proto package is being used.
-const _ = proto.ProtoPackageIsVersion4
 
 // Response message containing the gauge name and value
 type GaugeResponse struct {
