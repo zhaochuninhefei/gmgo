93a4bc3b2c015492c18168329d9a74689b4d55d3
zhaochuninhefei <zhaochun.hefei@qq.com>
2024-11-09 12:17:01 +0800

    优化代码

--- net/ipv6/unicast_test.go
+++ net/ipv6/unicast_test.go
@@ -96,9 +96,13 @@ func TestPacketConnReadWriteUnicastICMP(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer c.Close()
+	defer func(c net.PacketConn) {
+		_ = c.Close()
+	}(c)
 	p := ipv6.NewPacketConn(c)
-	defer p.Close()
+	defer func(p *ipv6.PacketConn) {
+		_ = p.Close()
+	}(p)
 
 	dst, err := net.ResolveIPAddr("ip6", "::1")
 	if err != nil {
