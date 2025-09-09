2b56468366bdf8170e296ae52f63d98557f34ef1
zhaochuninhefei <zhaochun.hefei@qq.com>
2025-09-09 08:14:05 +0800

    优化代码

--- x509/sec1.go
+++ x509/sec1.go
@@ -249,7 +249,7 @@ func marshalECPrivateKeyWithOID(key interface{}, oid asn1.ObjectIdentifier) ([]b
 			Version:       ecPrivKeyVersion,
 			PrivateKey:    priv.D.FillBytes(privateKey),
 			NamedCurveOID: oid,
-			PublicKey:     asn1.BitString{Bytes: elliptic.Marshal(priv.Curve, priv.X, priv.Y)},
+			PublicKey:     asn1.BitString{Bytes: gmelliptic.StdMarshal(priv.Curve, priv.X, priv.Y)},
 			PrivType:      "ecdsa",
 		})
 	case *ecdsa_ext.PrivateKey:
